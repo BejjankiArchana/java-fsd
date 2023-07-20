f24ff9cddaf6e8384496201df2d733d44417d6fd
BejjankiArchana <136701497+BejjankiArchana@users.noreply.github.com>
2023-07-20 13:12:57 +0530

    Add files via upload

---
+++ phase3/EventExample/src/main/java/com/event/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.event;
+public class HelloWorld {
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
