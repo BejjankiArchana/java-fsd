736703128d1e514cd5d8873e493cff6575cd4db9
BejjankiArchana <136701497+BejjankiArchana@users.noreply.github.com>
2023-07-27 11:06:15 +0530

    Add files via upload

---
+++ phase3/SpringBootRestDownloadExample/src/test/java/com/download/example/SpringBootRestDownloadExampleApplicationTests.java
@@ -0,0 +1,14 @@
+package com.download.example;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestDownloadExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
